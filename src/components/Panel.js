c1d12678e2889b817a6b2a5d0b172a7df353398d
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-09-19 19:28:52 -0400

    anime intro box test

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../components/Panel.css';
-import Anime from 'react-anime';
-
+import anime from 'animejs'
 let debounce = (func, wait, immediate) => {
   var timeout;
   return () => {
@@ -24,13 +23,22 @@ let winsize = { width: window.innerWidth, height: window.innerHeight }
 
 class Panel extends React.Component {
   componentDidMount() {
-    this.DOM = { el: document.getElementById('intro__box') }
-    this.boxRect = this.DOM.el.getBoundingClientRect()
+		this.DOM = {
+			el: document.getElementById('intro__box'),
+			enter: document.getElementById('intro__enter')
+	 }
+		// this.DOM.enter = document.getElementById('intro__enter');
+		console.log(this.DOM.enter);
 
+
+		this.animatableElems = Array.from(document.querySelectorAll('.animatable')).sort(() => 0.5 - Math.random());
+
+    this.boxRect = this.DOM.el.getBoundingClientRect()
     this.layout();
 
     this.isOpen = true;
-    this.initEvents();
+		this.initEvents();
+
   }
   layout() {
     this.DOM.el.style.transform = `scaleX(${winsize.width/this.boxRect.width}) scaleY(${winsize.height/this.boxRect.height})`
@@ -38,22 +46,42 @@ class Panel extends React.Component {
 
   }
   initEvents() {
+		this.DOM.enter.addEventListener('click', (ev) => {
+			ev.preventDefault();
+			this.close();
+		});
+
+
+		// Window resize
     this.onResize = () => {
-      winsize = { width: window.innerWidth, height: window.innerHeight};
+      winsize = { width: window.innerWidth, height: window.innerHeight };
       if (this.isOpen) {
         this.layout();
       }
     };
     window.addEventListener('resize', debounce(() => this.onResize(), 10))
-  }
+	}
+	close() {
+		anime({
+			targets: this.DOM.el,
+			duration: 1000,
+			scaleX: {value: 1, duration: 300, easing: 'easeOutQuad'},
+			scaleY: {value: 1, duration: 700, delay: 300, easing: 'easeOutExpo'},
+			complete: () => this.isAnimating = false
+		});
+	}
+
+
+
   render() {
     return(
       <div id="intro">
         <div id="intro__box"/>
-          <div id="intro__title-wrap">
-            <h1 id="intro__title"> Vonnoonv</h1>
-          </div>
+        <div id="intro__title-wrap">
+          <h1 id="intro__title"> Vonnoonv</h1>
+					<a id="intro__enter">Enter</a>
 
+        </div>
       </div>
     );
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Anime from 'react-anime';
 import './App.css';
 import Panel from '../components/Panel'
 
@@ -8,8 +7,6 @@ class App extends Component {
 
   constructor() {
     super();
-    console.log(Anime);
-
   }
 
   render() {
