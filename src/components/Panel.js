ef3203f9c677ac18cdc12aa1ed1100786b6fa780
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-10-21 22:21:18 -0400

    intro panel responsive styling

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -22,12 +22,15 @@ let winsize = { width: window.innerWidth, height: window.innerHeight }
 
 // this.DOM = { el: document.getElementById('intro__box') }
 
-
+const divStyle = {
+  background: 'blue'
+}
 class Panel extends React.Component {
   componentDidMount() {
 		this.DOM = {
 			el: document.getElementById('intro__box'),
-			enter: document.getElementById('intro__enter')
+      enter: document.getElementById('intro__enter'),
+			enter_mobile: document.getElementById('enter__mobile')
    }
 
     this.DOM.logoImg = document.querySelector('#icon--arrowup');
@@ -44,8 +47,8 @@ class Panel extends React.Component {
   layout() {
     this.DOM.el.style.transform = `scaleX(${winsize.width/this.boxRect.width}) scaleY(${winsize.height/this.boxRect.height})`
     // console.log(winsize.width/this.boxRect.width)
-
   }
+
   initEvents() {
 		this.DOM.enter.addEventListener('click', (ev) => {
 			ev.preventDefault();
@@ -53,10 +56,9 @@ class Panel extends React.Component {
 		});
 
     this.DOM.logoImg.addEventListener('click', (ev) => {
-      // ev.preventDefault();
+      ev.preventDefault();
       this.open();
     });
-
 
 		// Window resize
     this.onResize = () => {
@@ -108,6 +110,12 @@ class Panel extends React.Component {
     this.isOpen = true;
     this.isAnimating = true;
 
+    anime.remove(this.DOM.logoImg);
+    anime({
+      targets: this.DOM.logoImg,
+      translateY: [{value: '-400%', duration: 200, easing: 'easeOutQuad'}, {value: ['200%', '0%'], duration: 700, easing: 'easeOutExpo'}]
+    });
+
     anime.remove(this.animatableElems);
     anime({
       targets: this.animatableElems,
@@ -134,6 +142,7 @@ class Panel extends React.Component {
   }
 
   render() {
+
     return(
       <div id="intro">
         <div id="intro__box"/>
@@ -153,6 +162,13 @@ class Panel extends React.Component {
 
           <p id="location" className="animatable">Flatbush Brooklyn New York NY 11215</p>
 
+          <a id="email__link"
+            href="mailto:someone@example.com?Subject=Hello%20again"
+            target="_top"
+            className="animatable">
+            Mgourdinedevelopment@gmail.com
+          </a>
+
           <ul id="media__links" className="animatable">
             <li>
               <a href="" target="_blank">Github</a>
@@ -162,46 +178,36 @@ class Panel extends React.Component {
             </li>
           </ul>
 
-          <a id="email__link"
-            href="mailto:someone@example.com?Subject=Hello%20again"
-            target="_top"
-            className="animatable">
-            Mgourdinedevelopment@gmail.com
-          </a>
-
-
-          <div id="description-center">
+          <div id="description__center">
             <Row type="flex">
-              <Col span={9} offset={3}>
-                <h1 id="header" className="animatable">A COMPUTER PROGRAMMER WITH AN EYE FOR DESIGN</h1>
+              <Col offset={3} xs={{span:18, align: 'center'}} sm={10} md={14} lg={12}>
+                <h1 id="header" className="animatable">A COMPUTER PROGRAMMER WITH A LOVE FOR DESIGN</h1>
               </Col>
             </Row>
 
-            <Row gutter={18} type="flex">
-              <Col span={5} offset={3}>
+            <Row gutter={18} type="flex" justify="start">
+              <Col xs={9} md={5} lg={5} offset={3} >
                 <p id="description-1" className="animatable">
                   1. &emsp; Development
                   <br/>
                   <br/>
-                  A Fullstack Web developer with experience with  HMTL, CSS, Javascript, Node JS  and much more.  Experience  with REST API’s.
+                  A Fullstack Web developer with experience with  HMTL, CSS, Javascript, Node JS and much more. Experience with REST API’s.
                 </p>
               </Col>
-              <Col span={5}>
+
+              <Col xs={9} md={{span:5}} lg={5} >
                 <p id="description-1" className="animatable">
                   2. &emsp; Design
                   <br/>
                   <br/>
-                  Graphic and web design created using the latest design tools. I enjoy callorborate with other graphic artists and contributing to the work of others.              </p>
+                  Web design created using the latest design tools. Made using professional design concepts. I enjoy making digital and hand made art.</p>
+              </Col>
+              <Col xs={{span:18, offset:3}} sm={{span:9, offset:13}} md={{span:9, offset:0}} lg={9} align="top">
+                <img className="animatable" id="shape" src={require("../assets/shape.png")} alt="shape"/>
+                <a className="animatable" id="intro__enter">Enter</a>
               </Col>
             </Row>
           </div>
-          <Row >
-            <Col offset={12} span={10}>
-              <img className="animatable" id="shape" src={require("../assets/shape.png")} alt="shape"/>
-              <a className="animatable" id="intro__enter">Enter</a>
-            </Col>
-          </Row>
-
 
         </div>
       </div>
