0c2838303301ddabe33c7dc88fb60595b573b077
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-11-01 12:18:25 -0400

    remove masonry

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,29 +1,15 @@
 import  React from 'react';
 import '../gallery/Gallery.css';
-import Masonry from 'masonry-layout';
 
 class Gallery extends React.Component {
 
-  componentDidMount() {
-    var grid = document.querySelector('.grid');
-    var msnry = new Masonry( grid, {
-      // options...
-      itemSelector: '.grid-item',
-      columnWidth: 200
-    });
 
-    // init with selector
-    var msnry = new Masonry( '.grid', {
-      // options...
-    });
+  componentDidMount() {
   }
 
   render() {
     return(
-      <div className="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 200 }'>
-        <div className="grid-item">fsdf</div>
-        <div className="grid-item">fsdf</div>
-      </div>
+			<div></div>
     );
   }
 }
