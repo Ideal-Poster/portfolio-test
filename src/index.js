f47bad04d76043c39dbad0965ed4375ead4cd403
Malcolm Gourdine <malcolmgourdine@gmail.com>
2019-03-24 18:48:21 -0400

    remove redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
-import { Provider } from 'react-redux';
 
-import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
-store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "DECREMENT" });
-store.dispatch({ type: "RESET" });
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
