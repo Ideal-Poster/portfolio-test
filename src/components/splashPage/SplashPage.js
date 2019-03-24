7a5a4a4586d2b443dc6eb6a1d13a114376a7c9eb
Malcolm Gourdine <malcolmgourdine@gmail.com>
2019-03-24 18:53:57 -0400

    add splash page

---
+++ src/components/splashPage/SplashPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './SplashPage.css' ;
+
+class SplashPage extends Component {
+  render() {
+    return(
+      <div id="splash__page">
+        <div id="background">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SplashPage;
